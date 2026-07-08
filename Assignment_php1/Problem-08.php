aba7424f9ba076799141d93ce5928d6d8e580ec3
m0khtarwael <mokhtarwael10@gmail.com>
2026-07-08 17:37:57 +0300

    Add files via upload

---
+++ Assignment_php1/Problem-08.php
@@ -0,0 +1,19 @@
+<?php
+
+function RouteRandomPass($length) {
+    $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    $password = "";
+
+    for ($i = 0; $i < $length; $i++) {
+        $randomIndex = rand(0, strlen($characters) - 1);
+        $password .= $characters[$randomIndex];
+    }
+
+    return $password;
+}
+
+$number = 8;
+
+echo RouteRandomPass($number);
+
+?>
